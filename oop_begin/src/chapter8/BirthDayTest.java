7fed9a3f6d15abd5e86f52dcca42be27322a2a91
JungByungGi <qudrlwjd12@naver.com>
2021-07-27 08:44:02 +0900

    접근 제어 지시자 get/set 학습

---
+++ oop_begin/src/chapter8/BirthDayTest.java
@@ -0,0 +1,11 @@
+package chapter8;
+
+public class BirthDayTest {
+    public static void main(String[] args){
+        BirthDay date = new BirthDay();
+        date.setYear(2021);
+        date.setMonth(12);
+        date.setDay(5);
+        date.showDate();
+    }
+}
